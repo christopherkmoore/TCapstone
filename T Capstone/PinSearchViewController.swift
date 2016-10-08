f119c532fd562bcfea85b7b1dd26dc561e17976b
christopherkmoore <christopherkmoore1@gmailcom>
2016-10-07 22:24:51 -0700

    deleted commented code

--- T Capstone/PinSearchViewController.swift
+++ T Capstone/PinSearchViewController.swift
@@ -22,30 +22,10 @@ class PinSearchViewController: UIViewController, UITableViewDataSource, NSFetche
     var fetchedResultsQuotesController: NSFetchedResultsController<Quotes>!
     var fetchedResultsPlacesController: NSFetchedResultsController<Places>!
     
-    
-//    var insertedIndexPaths : [IndexPath]!
-//    var deletedIndexPaths : [IndexPath]!
-//    var updatedIndexPaths : [IndexPath]!
-    
     var sharedContext: NSManagedObjectContext {
         return CoreDataStackManager.sharedInstance().managedObjectContext
     }
     
-    // GOING TO NEED TO SEE IF THIS IS THE BEST WAY. LET USE AS DEFAULT FETCH
-    
-
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-//        activityIndicator.isHidden = false
-//        activityIndicator.startAnimating()
-//        convertPlaces()
-//        activityIndicator.isHidden = true
-//        activityIndicator.stopAnimating()
-    }
-    
- 
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -140,53 +120,4 @@ class PinSearchViewController: UIViewController, UITableViewDataSource, NSFetche
         let quote = fetchedResultsQuotesController.object(at: indexPath)
         
     }
-    
-//    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-//        tableView.beginUpdates()
-//    }
-//    
-//    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-//        switch type {
-//        case .insert:
-//            tableView.insertRows(at: [newIndexPath!], with: .automatic)
-//        case .delete:
-//            tableView.deleteRows(at: [indexPath!], with: .automatic)
-//        case .update:
-//            let cell = tableView.cellForRow(at: indexPath!) as! CustomTableViewCell
-//            configureCell(cell: cell, indexPath: indexPath!)
-//        case .move:
-//            tableView.deleteRows(at: [indexPath!], with: .automatic)
-//            tableView.insertRows(at: [newIndexPath!], with: .automatic)
-//        }
-//    }
-//    
-//    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-//        tableView.endUpdates()
-//    }
-    
-//    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-//        insertedIndexPaths = [IndexPath]()
-//        deletedIndexPaths = [IndexPath]()
-//        updatedIndexPaths = [IndexPath]()
-//    }
-//    
-//    func controller ( _ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-//        
-//        switch type {
-//        case .insert:
-//            insertedIndexPaths.append(newIndexPath!)
-//            break
-//        case .delete:
-//            deletedIndexPaths.append(indexPath!)
-//            //add code to delete coredata object
-//            
-//        case .update:
-//            updatedIndexPaths.append(indexPath!)
-//            
-//        default:
-//            break
-//            
-//            
-//        }
-//    }
 }
